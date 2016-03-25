0b97e7e7c0135b5b70f72acfd61a46f64425d7e4
Mykola Kruliv <MykolaKruliv@gmail.com>
2016-03-25 12:31:26 +0200

    Fix

--- roles/oms/files/addDataMySql.sql
+++ roles/oms/files/addDataMySql.sql
@@ -33,30 +33,20 @@ insert into Products(ProductDescription,ProductName,ProductPrice) values('produc
 insert into Products(ProductDescription,ProductName,ProductPrice) values('productDescription4','productName4',4);
 insert into Products(ProductDescription,ProductName,ProductPrice) values('productDescription5','productName5',5);
 
-insert into Users(Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(10,'mail','ivanka','horoshko','iva','qwerty',1,4,1);
-insert into Users(Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(10,'mail','myroslav','shram','myroslav','qwerty',2,3,1);
-insert into Users(Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(10,'mail','marko','bekhta','marko','qwerty',3,2,1);
-insert into Users(Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(10,'mail','vitalik','nobis','vitalik','qwerty',4,1,1);
-insert into Users(Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(10,'mail','orest','vovchack','orest','qwerty',1,4,1);
-insert into Users(Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(10,'mail','roman','nazarevuch','romanN','qwerty',2,3,1);
-insert into Users(Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(10,'mail','roman','svustyn','romanS','qwerty',3,2,1);
-insert into Users(Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(10,'mail','firstName1','lastName1','login1','qwerty',1,1,2);
-insert into Users(Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(10,'mail','firstName2','lastName2','login2','qwerty',1,1,3);
-insert into Users(Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(10,'mail','firstName3','lastName3','login3','qwerty',1,1,4);
-insert into Users(Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(10,'mail','firstName4','lastName4','login4','qwerty',1,2,4);
+insert into Users(IsUserActive,Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(1,10,'mail','admin','admin','admin','qwerty',1,4,1);
+insert into Users(IsUserActive,Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(1,10,'mail','myroslav','shram','myroslav','qwerty',2,3,2);
+insert into Users(IsUserActive,Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(1,10,'mail','marko','bekhta','marko','qwerty',3,2,3);
+insert into Users(IsUserActive,Balance,Email,FirstName,LastName,Login,Password,CustomerTypeRef,RegionRef,RoleRef) values(1,10,'mail','vitalik','nobis','vitalik','qwerty',4,1,4);
 
-insert into Orders(DeliveryDate,OrderDate,TotalPrice,Assigne,Customer,OrderStatusRef,MaxDiscount,OrderName,OrderNumber) values(0,0,10,1,10,3,11,'OrderName1',1);
-insert into Orders(DeliveryDate,OrderDate,TotalPrice,Assigne,Customer,OrderStatusRef,MaxDiscount,OrderName,OrderNumber) values(0,0,10,3,11,3,12,'OrderName2',2);
-insert into Orders(DeliveryDate,OrderDate,TotalPrice,Assigne,Customer,OrderStatusRef,MaxDiscount,OrderName,OrderNumber) values(0,0,10,7,10,2,13,'OrderName3',3);
-insert into Orders(DeliveryDate,OrderDate,TotalPrice,Assigne,Customer,OrderStatusRef,MaxDiscount,OrderName,OrderNumber) values(0,0,10,4,10,1,14,'OrderName4',4);
-insert into Orders(DeliveryDate,OrderDate,TotalPrice,Assigne,Customer,OrderStatusRef,MaxDiscount,OrderName,OrderNumber) values(0,0,10,2,11,4,15,'OrderName5',5);
-insert into Orders(DeliveryDate,OrderDate,TotalPrice,Assigne,Customer,OrderStatusRef,MaxDiscount,OrderName,OrderNumber) values(0,0,10,2,10,1,16,'OrderName6',6);
+insert into Orders(DeliveryDate,OrderDate,TotalPrice,Assigne,Customer,OrderStatusRef,MaxDiscount,OrderName,OrderNumber) values(0,0,10,1,4,3,11,'OrderName1',1);
+insert into Orders(DeliveryDate,OrderDate,TotalPrice,Assigne,Customer,OrderStatusRef,MaxDiscount,OrderName,OrderNumber) values(0,0,10,3,4,3,12,'OrderName2',2);
+insert into Orders(DeliveryDate,OrderDate,TotalPrice,Assigne,Customer,OrderStatusRef,MaxDiscount,OrderName,OrderNumber) values(0,0,10,4,4,1,14,'OrderName4',4);
+insert into Orders(DeliveryDate,OrderDate,TotalPrice,Assigne,Customer,OrderStatusRef,MaxDiscount,OrderName,OrderNumber) values(0,0,10,2,4,4,15,'OrderName5',5);
+insert into Orders(DeliveryDate,OrderDate,TotalPrice,Assigne,Customer,OrderStatusRef,MaxDiscount,OrderName,OrderNumber) values(0,0,10,2,4,1,16,'OrderName6',6);
 
 insert into OrderItems(Cost,ItemPrice,Quantity,DimensionRef,OrderRef,ProductRef) values(10,10,2,2,1,1);
 insert into OrderItems(Cost,ItemPrice,Quantity,DimensionRef,OrderRef,ProductRef) values(10,10,2,1,2,2);
 insert into OrderItems(Cost,ItemPrice,Quantity,DimensionRef,OrderRef,ProductRef) values(10,10,2,3,3,3);
 insert into OrderItems(Cost,ItemPrice,Quantity,DimensionRef,OrderRef,ProductRef) values(10,10,2,2,4,4);
-insert into OrderItems(Cost,ItemPrice,Quantity,DimensionRef,OrderRef,ProductRef) values(10,10,2,3,5,4);
-insert into OrderItems(Cost,ItemPrice,Quantity,DimensionRef,OrderRef,ProductRef) values(10,10,2,1,6,3);
 insert into OrderItems(Cost,ItemPrice,Quantity,DimensionRef,OrderRef,ProductRef) values(10,10,2,3,2,1);
 insert into OrderItems(Cost,ItemPrice,Quantity,DimensionRef,OrderRef,ProductRef) values(10,10,2,2,4,2);
